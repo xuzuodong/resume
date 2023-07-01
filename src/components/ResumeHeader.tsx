6fa4bf760c7067250355b580befbc29f0cb2bc14
许作栋 <hi@xzd.me>
2023-07-01 21:47:51 +0800

    style: change all icons to src from `react-icons`

--- src/components/ResumeHeader.tsx
+++ src/components/ResumeHeader.tsx
@@ -1,5 +1,6 @@
+import { MdOutlineLocationOn, MdOutlineMail, MdOutlinePhone } from 'react-icons/md'
+
 import { ResumeHeaderField } from './ResumeHeaderField'
-import { LocationIcon, MailIcon, PhoneIcon } from './Icon'
 
 export function ResumeHeader() {
     return (<div>
@@ -8,11 +9,11 @@ export function ResumeHeader() {
             flex flex-row flex-wrap justify-center items-center
             space-x-2 text-sm sm:text-base space-y-1 text-gray-500 dark:text-gray-300
         ">
-            <ResumeHeaderField value="+86 17826877949" icon={<PhoneIcon className="w-4 h-4"/>}/>
+            <ResumeHeaderField value="+86 17826877949" icon={<MdOutlinePhone className="w-4 h-4"/>}/>
             <span>·</span>
-            <ResumeHeaderField value="hi@xzd.me" icon={<MailIcon className="w-4 h-4"/>}/>
+            <ResumeHeaderField value="hi@xzd.me" icon={<MdOutlineMail className="w-4 h-4"/>}/>
             <span>·</span>
-            <ResumeHeaderField value="浙江 杭州" icon={<LocationIcon className="w-4 h-4"/>}/>
+            <ResumeHeaderField value="浙江 杭州" icon={<MdOutlineLocationOn className="w-4 h-4"/>}/>
         </div>
     </div>)
 }
