0c6b40b461366816c8d42c9c93d69d8b796f0e47
许作栋 <hi@xzd.me>
2023-07-02 14:26:10 +0800

    feat: add footer

---
+++ src/components/ResumeFooter.tsx
@@ -0,0 +1,41 @@
+import { AiFillGithub } from 'react-icons/ai'
+
+export function ResumeFooter() {
+    return <footer className="max-w-2xl text-center pb-8 pt-2 m-auto op-60">
+        <hr
+            className="border-black dark:border-white border-opacity-8 dark:border-opacity-8"
+        />
+        <div className="mt-8 flex justify-between items-center flex-wrap px-4">
+            <div className="flex flex-col space-y-2">
+                <div className="text-xs flex items-center space-x-1.5">
+                    <span>© {new Date().getFullYear()}</span>
+                    <span>Ted Xu</span>
+                    <a href="https://github.com/xuzuodong/resume" target="_blank" style={{ color: 'var(--text-color)' }}>
+                        <AiFillGithub size="16px" />
+                    </a>
+                </div>
+            </div>
+            <p className="text-xs inline">
+                Powered by
+                <span> </span>
+                <a
+                    href="https://nextjs.org/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    style={{ color: 'var(--text-color)' }}
+                >
+                    Next.js
+                </a>
+                <span> </span> &amp; <span> </span>
+                <a
+                    href="https://vercel.com/home"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    style={{ color: 'var(--text-color)' }}
+                >
+                    Vercel
+                </a>
+            </p>
+        </div>
+    </footer>
+}
