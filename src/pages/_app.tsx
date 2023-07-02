60b7c0f96c3f50c7ed1fe176cb0a06cccd931065
许作栋 <hi@xzd.me>
2023-07-02 15:09:36 +0800

    feat: add website icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,24 @@
 import { type AppType } from 'next/dist/shared/lib/utils'
 import '~/styles/global.scss'
 import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 
 import { Footer } from '~/components/Footer'
 import ButtonTheme from '~/components/ButtonTheme'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-    return <ThemeProvider attribute="class" enableSystem>
-        <ButtonTheme></ButtonTheme>
-        <div id="write">
-            <Component {...pageProps} />
-        </div>
-        <Footer />
-    </ThemeProvider>
+    return <>
+        <Head>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>" />
+        </Head>
+        <ThemeProvider attribute="class" enableSystem>
+            <ButtonTheme></ButtonTheme>
+            <div id="write">
+                <Component {...pageProps} />
+            </div>
+            <Footer />
+        </ThemeProvider>
+    </>
 }
 
 export default MyApp
